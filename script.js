9326e4cc4c865889b6ca264c722874875ddf1ca2
wissal <wissal.ouarsafi@gmail.com>
2020-04-10 12:04:40 -0700

    Finish the html File, insert the list of people into the DOM

---
+++ script.js
@@ -0,0 +1,40 @@
+const draggableList = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+  'Jeff Bezos',
+  'Bill Gates',
+  'Warren Buffett',
+  'Bernard Arnault',
+  'Carlos Slim Helu',
+  'Amancio Ortega',
+  'Larry Ellison',
+  'Mark Zuckerberg',
+  'Michael Bloomberg',
+  'Larry Page'
+];
+
+// Store listitems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+function createList(){
+    [...richestPeople].forEach((el, index) =>{
+       const li = document.createElement('li');
+       li.setAttribute('data-index', index);
+
+       li.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+            <p class="person-name">${el}</p>
+            <i class="fas fa-grip-lines"></i>
+        </div>
+       `
+       listItems.push(li);
+
+       draggableList.appendChild(li);
+    })
+}
