07ee8befc477b4500a3a55e142a5ad5ee412f9f1
wissal <wissal.ouarsafi@gmail.com>
2020-04-10 13:24:44 -0700

    Create drag and drop functionality

--- script.js
+++ script.js
@@ -41,4 +41,57 @@ function createList(){
 
             draggableList.appendChild(li);
     })
+
+    addEventListeners();
 }
+
+function dragStart() {
+     dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+  
+function dragEnter() {
+    this.classList.add('over');
+}
+  
+function dragLeave() {
+    this.classList.remove('over');
+}
+  
+function dragOver(e) {
+    e.preventDefault();
+}
+  
+function dragDrop() {
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+  
+    this.classList.remove('over');
+}
+
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+  
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+}
+
+
+function addEventListeners() {
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+  
+    draggables.forEach(draggable => {
+      draggable.addEventListener('dragstart', dragStart);
+    });
+  
+    dragListItems.forEach(item => {
+      item.addEventListener('dragover', dragOver);
+      item.addEventListener('drop', dragDrop);
+      item.addEventListener('dragenter', dragEnter);
+      item.addEventListener('dragleave', dragLeave);
+    });
+}
+  
